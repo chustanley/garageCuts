d1e91137b246a8ce609b0045f09d459b7b3eea8a
Stanley Chu <chustanleys@gmail.com>
2023-11-05 19:02:30 -0800

    firebase authentication installed

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,171 +1,37 @@
 import React, { useState } from "react";
+import { auth } from "../../server/firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import Signup from "./Signup.jsx";
 
 const Login = () => {
+  // Login Credentials
+  const [loginUser, setLoginUser] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  // Page and CSS conditional styling
   const [signUp, setSignUp] = useState(false);
-  const [wrongPassword, setWrongPassword] = useState(false);
-  const [missingPassword, setMissingPassword] = useState(false);
 
-  const checkCredentials = () => {
-    console.log("CHECKING CREDENTIALS...");
+  // Firebase error for displaying error
+  const [firebaseError, setFirebaseError] = useState(false);
 
-    const username = document.getElementById("createUser");
-    const firstPassword = document.getElementById("createPass1").value;
-    const copyOfFirstPassword = document.getElementById("createPass2").value;
-
-    //If user is already in database, warn user to create different username
-
-    if (firstPassword.length === 0 || copyOfFirstPassword.length === 0) {
-      setMissingPassword(true);
-      return;
-    }
-    setMissingPassword(false);
-
-    //If passwords dont match, warn user to check if passwords match
-    if (firstPassword !== copyOfFirstPassword) {
-      setWrongPassword(true);
-      return;
-    }
-    setWrongPassword(false);
-
-    setSignUp(false);
+  // Firebase authentication function
+  const loginAccount = () => {
+    signInWithEmailAndPassword(auth, loginUser, loginPassword)
+      .then((data) => {
+        if (!data) {
+          throw data;
+        }
+        //SEND TO NEXT PAGE?
+        setFirebaseError(false);
+      })
+      .catch((err) => {
+        setFirebaseError(true);
+      });
   };
 
   return signUp ? (
-    <div className="w-full min-h-screen items-center flex">
-      <div className="w-full h-full mx-auto flex justify-center items-center">
-        {/* LOGIN BOX  */}
-        <div className="shadow-xl shadow-gray-400 rounded-xl p-5 w-full h-[60%] bg-gray-200 max-w-[500px] m-5">
-          <h1>Garage Cuts</h1>
-          <div className="my-5 h-[80%] rounded-lg bg-slate-300 p-5 justify-center items-center">
-            {/* name and email  find out what breaks it*/}
-            <form className="sm:grid grid-cols-2 gap-4 py-2">
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-2">First Name</label>
-                <input
-                  id="name"
-                  className="border-2 rounded-lg p-3 flex border-gray-300"
-                  type="text"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-2">Last Name</label>
-                <input
-                  id="phonenumber"
-                  className="border-2 rounded-lg p-3 flex border-gray-300"
-                  type="text"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-2">Phone Number</label>
-                <input
-                  id="phonenumber"
-                  className="border-2 rounded-lg p-3 flex border-gray-300"
-                  type="text"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-2">Email</label>
-                <input
-                  id="phonenumber"
-                  className="border-2 rounded-lg p-3 flex border-gray-300"
-                  type="text"
-                />
-              </div>
-            </form>
-
-            {/* <div className="mt-1">
-              <label className="uppercase text-sm py-2">Email</label>
-              <input
-                id="createUser"
-                className="border-2 rounded-lg p-3 flex border-gray-300 w-full"
-                type="text"
-              />
-            </div> */}
-
-            <form className="w-full">
-              <div className="mt-10">
-                <div className="flex flex-col">
-                  <label className="uppercase text-sm py-2">Username</label>
-                  <input
-                    id="createUser"
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="text"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label className="uppercase text-sm py-2">Password</label>
-                  <input
-                    id="createPass1"
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="text"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label className="uppercase text-sm py-2">
-                    {" "}
-                    re-enter Password
-                  </label>
-                  <input
-                    id="createPass2"
-                    className="border-2 rounded-lg p-3 flex border-gray-300"
-                    type="text"
-                  />
-                </div>
-              </div>
-              <div>
-                {missingPassword ? (
-                  <div className="justify-center items-center flex text-xs my-3 text-red-600">
-                    Please fill out all of the information
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-
-              <div>
-                {wrongPassword ? (
-                  <div className="justify-center items-center flex text-xs my-3 text-red-600">
-                    Please make sure both passwords match
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-
-              <div className="flex justify-center">
-                {/* type = button stops the refresh  */}
-                <button
-                  type="button"
-                  className="px-5 py-2 mt-7 bg-slate-500 rounded-lg hover:bg-gradient-to-r from-[#ffb115] to-[#f6dc30] "
-                >
-                  <h4
-                    className="uppercase text-sm text-white"
-                    onClick={() => {
-                      checkCredentials();
-                    }}
-                  >
-                    sign up
-                  </h4>
-                </button>
-              </div>
-            </form>
-          </div>
-          <div>
-            <h4 className="text-xs">
-              Already have an account?{" "}
-              <div
-                className="text-blue-600 inline-block text-xs"
-                onClick={() => {
-                  setSignUp(false);
-                }}
-              >
-                Sign in
-              </div>
-            </h4>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Signup signUp={signUp} setSignUp={setSignUp} />
   ) : (
     <div className="w-full min-h-screen items-center flex">
       <div className="w-full h-full mx-auto flex justify-center items-center">
@@ -181,6 +47,10 @@ const Login = () => {
                     id="username"
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
+                    value={loginUser}
+                    onChange={(e) => {
+                      setLoginUser(e.target.value);
+                    }}
                   />
                 </div>
                 <div className="flex flex-col">
@@ -189,31 +59,54 @@ const Login = () => {
                     id="password"
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
+                    value={loginPassword}
+                    onChange={(e) => {
+                      setLoginPassword(e.target.value);
+                    }}
                   />
                 </div>
               </div>
               <div className="justify-center items-center flex text-xs my-3 text-blue-600">
                 Forgot your email or password?
               </div>
+              <div>
+                {firebaseError ? (
+                  <div className="justify-center items-center flex text-xs my-3 text-red-600">
+                    Invalid username or password. Please try again.
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
               <div className="flex justify-center">
-                <button className="px-5 py-2 mt-7 bg-slate-500 rounded-lg hover:bg-gradient-to-r from-[#ffb115] to-[#f6dc30] ">
+                <button
+                  className="px-5 py-2 mt-7 bg-slate-500 rounded-lg hover:bg-gradient-to-r from-[#ffb115] to-[#f6dc30]"
+                  onClick={() => {
+                    loginAccount();
+                  }}
+                >
                   <h4 className="uppercase text-sm text-white">sign in</h4>
                 </button>
               </div>
             </form>
           </div>
           <div>
-            <h4 className="text-xs">
-              Don't have an account?{" "}
-              <div
-                className="text-blue-600 inline-block text-xs"
-                onClick={() => {
-                  setSignUp(true);
-                }}
-              >
-                Sign up!
-              </div>
-            </h4>
+            <div>
+              <h4 className="text-xs">
+                Don't have an account?{" "}
+                <div
+                  className="text-blue-600 inline-block text-xs"
+                  onClick={() => {
+                    setSignUp(true);
+                  }}
+                >
+                  Sign up!
+                </div>
+              </h4>
+            </div>
+            <div>
+              <h1></h1>
+            </div>
           </div>
         </div>
       </div>
---
+++ client/components/Signup.jsx
@@ -0,0 +1,236 @@
+import React, { useState } from "react";
+import { auth } from "../../server/firebase.js";
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const Signup = (props) => {
+  //SIGNUP PAGE STYLE RENDERING
+  const [wrongPassword, setWrongPassword] = useState(false);
+  const [missingPassword, setMissingPassword] = useState(false);
+
+  //SIGNUP STATE RENDERING
+  const [username, setUsername] = useState("");
+  const [firstPassword, setFirstPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  //FIREBASE AUTH EMAIL ERROR
+  const [firebaseError, setFirebaseError] = useState(false);
+
+  //SIGN UP FUNCTION
+  const checkCredentials = () => {
+    console.log("CHECKING CREDENTIALS...");
+
+    console.log(
+      username,
+      firstPassword,
+      secondPassword,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+    );
+
+    //Reset the error status incase one of them was on before
+    setMissingPassword(false);
+    setWrongPassword(false);
+
+    //If user is already in database, warn user to create different username
+    if (firstPassword.length === 0 || secondPassword.length === 0) {
+      setMissingPassword(true);
+      return;
+    }
+    setMissingPassword(false);
+
+    //If passwords dont match, warn user to check if passwords match
+    if (firstPassword !== secondPassword) {
+      setWrongPassword(true);
+      return;
+    }
+    setWrongPassword(false);
+
+    createUserWithEmailAndPassword(auth, email, firstPassword)
+      .then((userCredentials) => {
+        if (!userCredentials) {
+          throw userCredentials;
+        }
+        setFirebaseError(false);
+        console.log("FIREBASE SUCCESS!!!", userCredentials);
+        props.setSignUp(false);
+      })
+      .catch((err) => {
+        setFirebaseError(true);
+        console.log("FIREBASE ERROR!!!!", err);
+      });
+  };
+
+  return (
+    <div className="w-full min-h-screen items-center flex">
+      <div className="w-full h-full mx-auto flex justify-center items-center">
+        {/* LOGIN BOX  */}
+        <div className="shadow-xl shadow-gray-400 rounded-xl p-5 w-full h-[60%] bg-gray-200 max-w-[500px] m-5">
+          <h1>Garage Cuts</h1>
+          <div className="my-5 h-[80%] rounded-lg bg-slate-300 p-5 justify-center items-center">
+            {/* name and email  find out what breaks it*/}
+            <form id="formOne" className="sm:grid grid-cols-2 gap-4 py-2">
+              <div className="flex flex-col">
+                <label className="uppercase text-sm py-2">First Name</label>
+                <input
+                  id="name"
+                  className="border-2 rounded-lg p-3 flex border-gray-300"
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="uppercase text-sm py-2">Last Name</label>
+                <input
+                  className="border-2 rounded-lg p-3 flex border-gray-300"
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="uppercase text-sm py-2">Phone Number</label>
+                <input
+                  className="border-2 rounded-lg p-3 flex border-gray-300"
+                  type="text"
+                  value={phoneNumber}
+                  onChange={(e) => {
+                    setPhoneNumber(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="uppercase text-sm py-2">Email</label>
+                <input
+                  className="border-2 rounded-lg p-3 flex border-gray-300"
+                  type="text"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+            </form>
+
+            <form id="formTwo" className="w-full">
+              <div className="mt-10">
+                <div className="flex flex-col">
+                  <label className="uppercase text-sm py-2">Username</label>
+                  <input
+                    id="createUser"
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    value={username}
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="uppercase text-sm py-2">Password</label>
+                  <input
+                    id="createPass1"
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    value={firstPassword}
+                    onChange={(e) => {
+                      setFirstPassword(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="uppercase text-sm py-2">
+                    {" "}
+                    re-enter Password
+                  </label>
+                  <input
+                    id="createPass2"
+                    className="border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                    value={secondPassword}
+                    onChange={(e) => {
+                      setSecondPassword(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  {missingPassword ? (
+                    <div className="justify-center items-center flex text-xs my-3 text-red-600">
+                      Please fill out all of the information
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+
+                <div>
+                  {wrongPassword ? (
+                    <div className="justify-center items-center flex text-xs my-3 text-red-600">
+                      Please make sure both passwords match
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <div>
+                  {firebaseError ? (
+                    <div className="justify-center items-center flex text-xs my-3 text-red-600">
+                      Email already in use, Please try a new email
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+
+              <div className="flex justify-center mt-3">
+                {/* type = button stops the refresh  */}
+                <button
+                  type="button"
+                  className="px-5 py-2 mt-7 bg-slate-500 rounded-lg hover:bg-gradient-to-r from-[#ffb115] to-[#f6dc30] "
+                  onClick={() => {
+                    checkCredentials();
+                  }}
+                >
+                  <h4 className="uppercase text-sm text-white">sign up</h4>
+                </button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <h4 className="text-xs">
+              Already have an account?{" "}
+              <div
+                className="text-blue-600 inline-block text-xs"
+                onClick={() => {
+                  props.setSignUp(false);
+                  setWrongPassword(false);
+                  setMissingPassword(false);
+                }}
+              >
+                Sign in
+              </div>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
